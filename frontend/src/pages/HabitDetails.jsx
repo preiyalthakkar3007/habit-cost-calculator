ccc323ca0698965527c08d25c249f1abefb94cf4
Preiyal Thakkar <preiyalthakkar3007@gmail.com>
2026-02-18 14:12:52 -0800

    Fix React Hook eslint warning

--- frontend/src/pages/HabitDetails.jsx
+++ frontend/src/pages/HabitDetails.jsx
@@ -14,6 +14,7 @@ const HabitDetails = () => {
   
   useEffect(() => {
     fetchHabit();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [id]);
   
   const fetchHabit = async () => {
